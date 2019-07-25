d9bab07e0c8f0f049bd3a00ddcf909ab5ab481ca
brentongalien981 <brentongalien981@gmail.com>
2019-07-25 13:19:50 -0400

    Initial commit

---
+++ html/myprofile/privacypolicy.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>My Profile</title>
+<style type="text/css">
+.AllText {
+	font-family: "Arial Black", Gadget, sans-serif;
+	font-size: 12px;
+}
+.SmallText {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 10px;
+}
+body {
+	background-color: #333;
+}
+.FooterTextWhite {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: x-small;
+	font-weight: lighter;
+	color: #FFF;
+	font-style: normal;
+}
+.FooterTextGreen {
+	font-family: Arial, Helvetica, sans-serif;
+	color: #9aac86;
+	font-size: x-small;
+}
+a {
+	font-family: Arial, Helvetica, sans-serif;
+	text-decoration: underline;
+	color: #666;
+}
+</style>
+</head>
+
+<body>
+<?php
+	//GRAB THE POSTED EMAIL ADDRESS IF ANY
+	$email_address = $_GET['email'];
+	
+?>
+	
+<table width="700" border="0" cellpadding="0" cellspacing="0" align="center">
+  <tr>
+    <td><img src="images/HEADER-AVEDA.jpg" width="700" height="100" /></td>
+  </tr>
+  <tr>
+    <td bgcolor="#FFFFFF">
+    <form method="post" action="http://enews.avedacanada.ca/q/9O0o4qFEggJEUt64S6cBPOwWGdVcKSbAaR">
+      <table width="100%" border="0" cellspacing="3" cellpadding="3">
+        <tr>
+          <td><p class="SmallText">Privacy of personal information is of paramount importance to Aveda Canada.  We are committed to collecting, using and disclosing personal information  responsibly and only to the extent necessary with respect to the goods and  services we provide currently and in the future. This document describes our  privacy policies. <br />
+            <br />
+              <strong>Personal Information Collection and Use</strong><br />
+              Personal information is information about an identifiable individual.  Personal information includes information that relates to your personal  characteristics including, but not limited to, gender, age, birth date, income,  home address or phone number, email address, service/product use and history.  Personal information is to be contrasted with business information (e.g., an  individual's business address and telephone number), which is not protected by  privacy legislation. When you submit any personal information through this  website, you are consenting to its use by Aveda Canada for the purposes set  forth in this privacy policy. Information collected may be used for the  purposes of delivering emails containing promotional material, surveys,  announcements or other communication items. As well, this information may be  used in aggregate form for internal business purposes - data analytics,  developing marketing strategies and tracking website statistics.<br />
+              <strong><br />
+              Third Parties </strong><br />
+              Aveda Canada may, from time to time, retain the services of consultants and  agencies that will, in the course of their duties, have limited access to  personal information we hold. We restrict their access to any held personal  information as much as is reasonably possible, without hindering the process  for which they have been engaged by us. We also have their assurance that they  follow appropriate privacy principles and not use any collected information for  any other purpose other than those specifically relating to the on-going  business activities of Aveda Canada<br />
+              <br />
+              <strong>Links</strong><br />
+              From time to time avedacanada.com may contain links to or from other  websites. We are not responsible for the privacy practices of those websites or  their affiliated organizations/partners. Our privacy policy is in effect only  for information that is collected from this site. <br />
+              <strong><br />
+              Opt-Out</strong><br />
+              Regardless of any personal information supplied to us, you have the right to  opt out of receiving any future communications from us. All marketing  initiatives sent electronically will always have an unsubscribe link within the  body of the document. You may also have your name removed from our mailing list  by completing the form here (avedacanada.com/myprofile)<br />
+              <strong><br />
+              Disclaimer </strong><br />
+            On occasion, Aveda Canada may make changes or updates to website information  including product/service descriptions and pricing and reserves the right to  correct errors, inaccuracies and omissions at any time without prior  notification. </p></td>
+        </tr>
+      </table>
+    </form>
+    </td>
+  </tr>
+  <tr>
+    <td align="center" bgcolor="#9aac86"><p class="FooterTextWhite">&nbsp;</p>
+      <p class="FooterTextWhite">Aveda® is a registered trademark of the Aveda Corporation Inc. licensed to Collega Retail (Canada) Inc. for use in Canada.<br />
+      </p>
+      <p class="FooterTextWhite"><a href="http://www.avedacanada.com/myprofile">Manage My Profile</a></p>
+      <p class="FooterTextWhite">If you have any questions, please email us at <a href="mailto:info@collega.com">info@collega.com</a>.<br />
+    Collega Retail (Canada) Inc., 210 Lesmill Road, Toronto, ON  M3B 2T5</p>
+    <p class="FooterTextWhite">&nbsp;</p></td>
+  </tr>
+</table>
+</body>
+</html>
